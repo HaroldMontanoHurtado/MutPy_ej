3491ef6d86260f053181fbb16272009f28cdbb28
HaroldMontanoHurtado <harold.montano@correounivalle.edu.co>
2024-04-01 02:55:01 -0500

    agrega bucle while

--- src/main.py
+++ src/main.py
@@ -1,33 +1,37 @@
 from db.consultas import *
 from test.test_crud import *
+from sys import exit
 
 print(' - - - - CRUD - - - -')
 print('1. Añadir empleados.')
 print('2. Añadir cliente.')
 print('3. Añadir ventas.')
 print('4. Reporte de ventas.')
+print('5. Salir.')
 
-text = int(input('Digite: '))
+while(True):
+    opc = int(input('Digite: '))
 
-if (text==1):
-    imprimir_empleados()
-    text = input('Nombre empleado: ')
-    add_empleado(text)
-    imprimir_empleados()
-    
-elif (text==2):
-    imprimir_clientes()
-    text = input('Nombre cliente: ')
-    add_cliente(text)
-    imprimir_clientes()
-    
-elif (text==3):
-    text = input('Nombre producto: ')
-    id = int(input('id empleado: '))
-    add_venta(text, id)
-    
-elif (text==4):
-    reporte_venta()
-else:
-    pass
-
+    if (opc==1):
+        imprimir_empleados()
+        txt = input('Nombre empleado: ')
+        add_empleado(txt)
+        imprimir_empleados()
+        
+    elif (opc==2):
+        imprimir_clientes()
+        txt = input('Nombre cliente: ')
+        add_cliente(txt)
+        imprimir_clientes()
+        
+    elif (opc==3):
+        txt = input('Nombre producto: ')
+        id = int(input('id empleado: '))
+        add_venta(txt, id)
+        
+    elif (opc==4):
+        reporte_venta()
+    elif (opc==5):
+        exit()
+    else:
+        pass
